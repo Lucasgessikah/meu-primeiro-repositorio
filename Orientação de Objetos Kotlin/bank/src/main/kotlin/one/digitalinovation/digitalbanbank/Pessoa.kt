072b66b1d52ed650be1b55431daeb71dab6ab688
Lucasgessikah <104868677+Lucasgessikah@users.noreply.github.com>
2022-06-07 20:22:31 -0300

    adicionando tudo

--- Orientação de Objetos Kotlin/bank/src/main/kotlin/one/digitalinovation/digitalbanbank/Pessoa.kt
+++ Orientação de Objetos Kotlin/bank/src/main/kotlin/one/digitalinovation/digitalbanbank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinovation.digitalbanbank
 
 class Pessoa {
+    var nome: String = "Lucas"
+    var cpf: String = "056.498.743-36"
+}
+
+fun main() {
+    val lucas =Pessoa()
+
+    println(lucas.nome)
+    println(lucas.cpf)
 }
